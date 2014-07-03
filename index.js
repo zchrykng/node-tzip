d49b1e78b19b2246ee0ff3792c98fb6f98316f67
Zachary King <zchrykng@gmail.com>
2014-07-03 15:44:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+var data = require('./data');
+
+var mapping = {};
+
+data.zones.forEach(function (key) {
+  data[key].forEach(function (zip) {
+    mapping[zip] = (key == 'unused') ? null : key;
+  })
+});
+
+module.exports = function (zip) {
+  if (typeof zip == 'number') {
+    zip = ('00000' + zip).slice(-5);
+  } else if (!(typeof zip == 'string' || zip instanceof String)) {
+    console.log('TZIP: This function only accepts numbers and strings. Pleas correct this.');
+    console.log(zip);
+  }
+  for (var i = 5; i >= 0; i--) {
+    var checkString = zip.substr(0, i);
+    if (mapping[checkString]) {
+      return mapping[checkString]
+    }
+  }
+  return false;
+};
