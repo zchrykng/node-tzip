f83a494319d17cf0423912df67be92d3e0938410
Zachary King <zchrykng@gmail.com>
2014-07-03 16:31:00 -0400

    add handling for broken strings

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ data.zones.forEach(function (key) {
 });
 
 module.exports = function (zip) {
-  if (typeof zip == 'number') {
+  if (typeof zip == 'number' || zip.length < 5) {
     zip = ('00000' + zip).slice(-5);
   } else if (!(typeof zip == 'string' || zip instanceof String)) {
     console.log('TZIP: This function only accepts numbers and strings. Pleas correct this.');
     console.log(zip);
+    return false;
   }
   for (var i = 5; i >= 0; i--) {
     var checkString = zip.substr(0, i);
